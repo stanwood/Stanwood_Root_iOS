2c3aef52b5908ebeaba3e296debf13bb3d18e6ee
Ronan O Ciosoig <ronan.o.ciosoig@stanwood.de>
2018-02-22 07:38:47 +0100

    Initial commit.

---
+++ Example/Pods/Target Support Files/Stanwood/Stanwood-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Stanwood : NSObject
+@end
+@implementation PodsDummy_Stanwood
+@end
---
+++ Example/Stanwood/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Stanwood
+//
+//  Created by ronanoc@icloud.com on 02/17/2018.
+//  Copyright (c) 2018 ronanoc@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
